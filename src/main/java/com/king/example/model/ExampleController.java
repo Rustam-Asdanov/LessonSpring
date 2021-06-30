96600ca8a584089a4c5db6ec8030add6e425e510
Krosent <krosent2@gmail.com>
2021-07-01 00:16:06 +0200

    Adding example model to demonstrate how serialization works in Spring Boot.

--- src/main/java/com/king/example/model/ExampleController.java
+++ src/main/java/com/king/example/model/ExampleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CustomerModel {
+public class ExampleController {
 
     @GetMapping("/fetchRequest")
     public ExampleModel returnResult(Model model){
