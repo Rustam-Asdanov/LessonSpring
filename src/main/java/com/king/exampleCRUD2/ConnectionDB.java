af83578d223dccfea29b56849b3f4cef4d4b817d
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-07-05 23:54:16 +0400

    Class connection is created

--- src/main/java/com/king/exampleCRUD2/ConnectionDB.java
+++ src/main/java/com/king/exampleCRUD2/ConnectionDB.java
@@ -1,11 +1,14 @@
 package com.king.exampleCRUD2;
 
+import org.springframework.stereotype.Component;
+
 import javax.swing.plaf.nimbus.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Component
 public class ConnectionDB {
     private final String URL = "jdbc:mysql://localhost:3310/example";
     private final String LOGIN = "root";
