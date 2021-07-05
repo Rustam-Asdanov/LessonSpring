3a2700d588fc7397a118d7cc5314cde173621906
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-07-05 23:53:49 +0400

    Class connection is created

---
+++ src/main/java/com/king/exampleCRUD2/ConnectionDB.java
@@ -0,0 +1,28 @@
+package com.king.exampleCRUD2;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionDB {
+    private final String URL = "jdbc:mysql://localhost:3310/example";
+    private final String LOGIN = "root";
+    private final String PASSWORD = "11111";
+    private Connection conn;
+    private Statement statement;
+
+    {
+        try {
+            conn = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            statement = conn.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public Statement getStatement(){
+        return statement;
+    }
+}
